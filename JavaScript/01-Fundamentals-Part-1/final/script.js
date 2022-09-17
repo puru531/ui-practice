008226b28e981f7792a00813a95bd610386fb61e
Purushottam Kumar <purushottam1619@gmail.com>
2022-09-18 00:47:29 +0530

    Variables and Values

---
+++ JavaScript/01-Fundamentals-Part-1/final/script.js
@@ -0,0 +1,39 @@
+// alert("Hello Moto!")
+
+console.log("Purushottam");
+console.log(27 + 27 + 12 - 12 - 2);
+
+// ----------------------------> Variables and Values <----------------------------
+let firstName = "Purushottam";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable naming convension --> can only contains letters, numbers or _ and $
+//should start with alphabets or _ or $ and should not start with a number. should not start with capital letter
+//should not use reserved javascript keywords (example - new, function )
+//constant value's variable name should be in uppercase
+//we should make the variable name descriptive
+let first;
+let firstNamePerson; //preferred in Javascript
+let first_name;
+let _firstName;
+let $first_Name;
+
+//Assignment
+// 1.Declare variables called 'country', 'continent'and 'population'and assign their values according to your own country(population in millions)
+let country = "India";
+let continent = "Asia";
+let population = 1400;
+
+// 2.Log their values to the console
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// ----------------------------> Data Types <----------------------------
+//In JavaScript all values are into two categories --> Object and Primitive
+
+//Primitive
