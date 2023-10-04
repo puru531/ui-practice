80e7986bf879bdf27c2a4ab14a1efbd2278e47db
puru531 <purushottam1619@gmail.com>
2023-10-05 03:06:33 +0530

    react-router

---
+++ react/worldwise/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+
+function App() {
+  return (
+    // <div> //can also add html around router components
+    //   <h1>Hello Router</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+    // </div>
+  );
+}
+
+export default App;
