84608cb05f09be1874547f80b24be0cbb5660959
puru531 <purushottam1619@gmail.com>
2023-10-06 02:19:31 +0530

    Routing and CSS modules

---
+++ react/worldwise/src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components V1/AppNav";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <p>App</p>
+    </div>
+  );
+}
+
+export default AppLayout;
--- react/worldwise/src/pages/PageNotFound.jsx
+++ react/worldwise/src/pages/PageNotFound.jsx
@@ -1,9 +1,7 @@
-function PageNotFound() {
+export default function PageNotFound() {
   return (
     <div>
-      <h1>Page not found :(</h1>
+      <h1>Page not found 😢</h1>
     </div>
   );
 }
-
-export default PageNotFound;
