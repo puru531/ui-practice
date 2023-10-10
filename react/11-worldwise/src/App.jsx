2fb97e2500300609a5d0b5c25f3e47998a8a6c9b
puru531 <purushottam1619@gmail.com>
2023-10-11 02:06:25 +0530

    react router

--- react/11-worldwise/src/App.jsx
+++ react/11-worldwise/src/App.jsx
@@ -5,18 +5,61 @@ import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import { useState } from "react";
+import { useEffect } from "react";
+import CountryList from "./components/CountryList";
+
+const BASE_URL = "http://localhost:8000";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("Failed to fetch cities");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     // <div> //can also add html around router components
     //   <h1>Hello Router</h1>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        {/* <Route path="/" element={<Homepage />} /> */}
+        {/* default page */}
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          {/* Index route : A default route that is loaded when none of the child matches the router */}
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          {/* children routes */}
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- react/11-worldwise/src/components/City.jsx
+++ react/11-worldwise/src/components/City.jsx
@@ -5,7 +5,7 @@ const formatDate = (date) =>
     day: "numeric",
     month: "long",
     year: "numeric",
-    weekday: "long",
+    // weekday: "long",
   }).format(new Date(date));
 
 function City() {
