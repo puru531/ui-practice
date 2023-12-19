029f0efc9e9a5f93d993c01b37bc9ab682987b66
Purushottam Kumar <purushottam1619@gmail.com>
2023-12-19 07:48:34 +0530

    Performance optimization

--- react/12-atomic-blog/src/Test.js
+++ react/12-atomic-blog/src/Test.js
@@ -14,13 +14,34 @@ function SlowComponent() {
   );
 }
 
-export default function Test() {
+function Counter({ children }) {
   const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+      {children}
+    </div>
+  );
+}
+
+export default function Test() {
+  // In below code we are using SlowComponent as child elemnt which causes this component to take longer time to re-render on count update. So if we pass them as children then slowcomponent will not re-render and will not cause any effect to current component.
+
+  // const [count, setCount] = useState(0);
+  // return (
+  //   <div>
+  //     <h1>Slow counter?!?</h1>
+  //     <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+  //     <SlowComponent />
+  //   </div>
+  // );
+  return (
+    <div>
+      <h1>Slow counter?!?</h1>
+      <Counter>
+        <SlowComponent />
+      </Counter>
     </div>
   );
 }
