cc9e9c6ce68b59c2ac326fad352c94792273f9a1
Purushottam Kumar <purushottam1619@gmail.com>
2024-01-02 01:07:04 +0530

    tailwind css

--- react/16-fast-react-pizza/tailwind.config.js
+++ react/16-fast-react-pizza/tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'Roboto Mono, monospace',
+    },
+
+    extend: {
+      //if want keep everything of tailwing but want to extend something from ypour side and ass inside extent{}
+      colors: {
+        pizza: '#123456',
+      },
+      height: {
+        screen: '100dvh', //dynamic viewport height, work nice in mobile also.
+      },
+    },
   },
   plugins: [],
 };
