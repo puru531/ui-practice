c892448e518b86937142094685222ccb53d82582
Purushottam Kumar <purushottam1619@gmail.com>
2024-01-29 02:45:13 +0530

    update user

--- react/17-the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ react/17-the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("puru@example.com");
-  const [password, setPassword] = useState("Pass@123");
+  const [email, setEmail] = useState("test@test.com");
+  const [password, setPassword] = useState("12345678");
 
   const { login, isLoading } = useLogin();
 
--- react/17-the-wild-oasis/src/features/authentication/SignupForm.jsx
+++ react/17-the-wild-oasis/src/features/authentication/SignupForm.jsx
@@ -3,14 +3,22 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useSignup } from "./useSignup";
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-  const { register, formState, getValues, handleSubmit } = useForm();
-  const errors = formState;
-  function onSubmit(data) {
-    console.log(data);
+  const { signup, isLoading } = useSignup();
+  const { register, formState, getValues, handleSubmit, reset } = useForm();
+  const { errors } = formState;
+
+  function onSubmit({ fullName, email, password }) {
+    signup(
+      { fullName, email, password },
+      {
+        onSettled: () => reset(),
+      }
+    );
   }
 
   return (
@@ -19,13 +27,8 @@ function SignupForm() {
         <Input
           type="text"
           id="fullName"
-          {...register("fullName", {
-            required: "This field is required",
-            pattern: {
-              value: /\S+@\S+\.\S+/,
-              message: "Please provide a valid email address!",
-            },
-          })}
+          disabled={isLoading}
+          {...register("fullName", { required: "This field is required" })}
         />
       </FormRow>
 
@@ -33,11 +36,12 @@ function SignupForm() {
         <Input
           type="email"
           id="email"
+          disabled={isLoading}
           {...register("email", {
             required: "This field is required",
-            minLength: {
-              value: 8,
-              message: "Password needs a minimum of 8 characters",
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "Please provide a valid email address",
             },
           })}
         />
@@ -50,10 +54,13 @@ function SignupForm() {
         <Input
           type="password"
           id="password"
+          disabled={isLoading}
           {...register("password", {
             required: "This field is required",
-            validate: (value) =>
-              value === getValues().password || "Passwords needs to match!",
+            minLength: {
+              value: 8,
+              message: "Password needs a minimum of 8 characters",
+            },
           })}
         />
       </FormRow>
@@ -62,18 +69,26 @@ function SignupForm() {
         <Input
           type="password"
           id="passwordConfirm"
+          disabled={isLoading}
           {...register("passwordConfirm", {
             required: "This field is required",
+            validate: (value) =>
+              value === getValues().password || "Passwords need to match",
           })}
         />
       </FormRow>
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button
+          variation="secondary"
+          type="reset"
+          disabled={isLoading}
+          onClick={reset}
+        >
           Cancel
         </Button>
-        <Button>Create new user</Button>
+        <Button disabled={isLoading}>Create new user</Button>
       </FormRow>
     </Form>
   );
