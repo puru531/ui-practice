4ec7252cbda21c05fd346f4342ca74e34277be6c
Purushottam Kumar <purushottam1619@gmail.com>
2024-02-03 04:12:42 +0530

    final touch

--- react/17-the-wild-oasis/src/features/bookings/BookingDetail.jsx
+++ react/17-the-wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -16,6 +16,7 @@ import { useCheckout } from "./useCheckout";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
 import { useDeleteBooking } from "./useDeleteBooking";
+import Empty from "../../ui/Empty";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -32,6 +33,8 @@ function BookingDetail() {
   const navigate = useNavigate();
 
   if (isLoading) return <Spinner />;
+
+  if (!booking) return <Empty resourceName="booking" />;
 
   const { status, id: bookingId } = booking;
 
