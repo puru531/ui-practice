6054a44cc5c7432bb6a8e8fcf201a658149de257
Purushottam Kumar <purushottam1619@gmail.com>
2024-02-03 03:38:52 +0530

    recharts

--- react/17-the-wild-oasis/src/services/apiBookings.js
+++ react/17-the-wild-oasis/src/services/apiBookings.js
@@ -52,6 +52,7 @@ export async function getBooking(id) {
 }
 
 // Returns all BOOKINGS that are were created after the given date. Useful to get bookings created in the last 30 days, for example.
+//Date should be ISOString
 export async function getBookingsAfterDate(date) {
   const { data, error } = await supabase
     .from("bookings")
