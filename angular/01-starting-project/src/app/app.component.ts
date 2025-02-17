8457c772a21f529698576bdf925b6620ab7565c0
Purushottam <purushottam1619@gmail.com>
2025-02-18 00:11:26 +0530

    refactor

--- angular/01-starting-project/src/app/app.component.ts
+++ angular/01-starting-project/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import {HeaderComponent} from "./header.component";
+import {HeaderComponent} from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import {DUMMY_USERS} from "./dummy-users";
+import {TasksComponent} from "./tasks/tasks.component";
+// import {NgFor, NgIf} from "@angular/common"; // NgFor and NgIf are directives that are used in the template of AppComponent --> Older version
 
 /**
  * In TypeScript and Angular, a decorator is a special kind of declaration that can be attached to a class, method, accessor, property, or parameter.
@@ -11,8 +15,22 @@ import {HeaderComponent} from "./header.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent], // this is needed to import the HeaderComponent in the AppComponent as HeaderComponent is used in the template of AppComponent and is a standalone component.
+  // imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf], // NgFor, NgIf are needed in imports as they are used in the template of AppComponent
+  imports: [HeaderComponent, UserComponent, TasksComponent], // this is needed to import the HeaderComponent in the AppComponent as HeaderComponent is used in the template of AppComponent and is a standalone component.
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectedUserId?: string;
+
+  get selectedUser() {
+    return this.users.find(user => user.id === this.selectedUserId)!; // ! is used to tell TypeScript that the property will be initialized later
+  }
+
+  onSelectUser(id: string) {
+    console.log('User selected with id: ' + id);
+    this.selectedUserId = id;
+  }
+
+}
--- angular/01-starting-project/src/main.ts
+++ angular/01-starting-project/src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import {HeaderComponent} from "./app/header.component";
+import {HeaderComponent} from "./app/header/header.component";
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
 // bootstrapApplication(HeaderComponent) // this is needed to bootstrap the header component if we are using it in the index.html file
