9f5fef2cedbfa78f1061237f9a899cc3a5684226
Purushottam <purushottam1619@gmail.com>
2025-03-01 02:14:12 +0530

    reusable component

---
+++ angular/03-component-deep-dive/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, input, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-item.component.html',
+  styleUrl: './dashboard-item.component.css',
+})
+export class DashboardItemComponent {
+  @Input({ required: true }) image!: { src: string; alt: string };
+  // @Input({ required: true }) dashboardTitle!: string;
+  dashboardTitle = input.required<string>();
+}
