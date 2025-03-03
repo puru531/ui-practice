da16e774c40bc88e2b259c378a55d8d829af7454
Purushottam <purushottam1619@gmail.com>
2025-03-04 00:01:08 +0530

    host element

--- angular/03-component-deep-dive/src/app/shared/control/control.component.ts
+++ angular/03-component-deep-dive/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import {Component, input, ViewEncapsulation} from '@angular/core';
+import {Component, ElementRef, inject, input, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -6,9 +6,25 @@ import {Component, input, ViewEncapsulation} from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None // Disable style scoping of the component css
+  encapsulation: ViewEncapsulation.None, // Disable style scoping of the component css
+  host: {
+    class: 'control', //@HostBinding can also set the class to host element. -> but NOT encouraged
+    '(click)': 'onClick()' // @HostListener used to listen to host events --> but not encouraged
+  }
 })
 export class ControlComponent {
+  // @HostBinding('class') className= "control"
   label = input.required<string>()
+
+  // @HostListener('click') handleClick() { // NOT recommended to use
+  //   console.log('Control clicked')
+  // }
+
+  private el = inject(ElementRef); // gives access to host element of the component
+
+  onClick() {
+    console.log('Control clicked');
+    console.log(this.el.nativeElement);
+  }
 
 }
