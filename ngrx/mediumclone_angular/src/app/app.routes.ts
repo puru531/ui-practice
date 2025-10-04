a60cfbed35000f366dd3faac8e52a71fe6831711
Purushottam <purushottam1619@gmail.com>
2025-10-04 23:00:41 +0530

    routing configuration

---
+++ ngrx/mediumclone_angular/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Route } from '@angular/router';
+
+export const appRoutes: Route[] = [
+  {
+    path: 'register',
+    // lazy load auth
+    loadChildren: () =>
+      import('src/app/auth/auth.routes').then((m) => m.registerRoutes),
+  },
+];
--- ngrx/mediumclone_angular/src/main.ts
+++ ngrx/mediumclone_angular/src/main.ts
@@ -9,10 +9,12 @@ platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 */
 
-
 // -------------- Standalone component based application bootstrap --------------
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, {providers: []})
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(appRoutes)],
+}).catch((err) => console.error(err));
