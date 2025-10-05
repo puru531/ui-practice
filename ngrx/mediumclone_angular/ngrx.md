c3f21aa3a92be012e412aad398b795de7f1a1f76
Purushottam <purushottam1619@gmail.com>
2025-10-05 10:03:30 +0530

    installling and configuring Devtools

--- ngrx/mediumclone_angular/ngrx.md
+++ ngrx/mediumclone_angular/ngrx.md
@@ -4,3 +4,9 @@ npm i @ngrx/store@15
 # v15 because this project uses angular 15
 ```
 
+Install Devtools:
+First install Redux devtools in browser from extension store.
+
+```bash
+npm i @ngrx/store-devtools@15
+```
--- ngrx/mediumclone_angular/src/main.ts
+++ ngrx/mediumclone_angular/src/main.ts
@@ -15,7 +15,18 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app/app.routes';
 import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { isDevMode } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes), provideStore()],
+  providers: [
+    provideRouter(appRoutes),
+    provideStore(),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      trace: false,
+      traceLimit: 75,
+    }),
+  ],
 }).catch((err) => console.error(err));
