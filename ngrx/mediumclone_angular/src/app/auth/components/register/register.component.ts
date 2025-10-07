61763a701026f5ba5ed698f373ea1438ceb296c6
Purushottam <purushottam1619@gmail.com>
2025-10-07 23:11:47 +0530

    showing error messages

--- ngrx/mediumclone_angular/src/app/auth/components/register/register.component.ts
+++ ngrx/mediumclone_angular/src/app/auth/components/register/register.component.ts
@@ -5,13 +5,23 @@ import { Store } from '@ngrx/store';
 import { authActions } from '../../store/actions';
 import { RegisterRequestInterface } from '../../types/registerRequest.inteface';
 import { CommonModule } from '@angular/common';
-import { selectIsSubmitting } from '../../store/reducers';
+import {
+  selectIsSubmitting,
+  selectValidationErrors,
+} from '../../store/reducers';
+import { combineLatest } from 'rxjs';
+import { BackendErrorMessages } from 'src/app/shared/components/backendErrorMessages/backendErrorMessages.component';
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
   standalone: true,
-  imports: [CommonModule, RouterLink, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+    ReactiveFormsModule,
+    BackendErrorMessages,
+  ],
 })
 export class RegisterComponent {
   form = this.fb.nonNullable.group({
@@ -19,7 +29,16 @@ export class RegisterComponent {
     email: ['', Validators.required],
     password: ['', Validators.required],
   });
+
+  /* ------------------- Group with combinelatest ---------
   isSubmitting$ = this.store.select(selectIsSubmitting);
+  backendErrors$ = this.store.select(selectValidationErrors);
+*/
+
+  data$ = combineLatest({
+    isSubmitting: this.store.select(selectIsSubmitting),
+    backendErrors: this.store.select(selectValidationErrors),
+  });
 
   constructor(
     private fb: FormBuilder,
